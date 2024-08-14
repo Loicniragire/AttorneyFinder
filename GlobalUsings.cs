80cdc6823be0fcf06929bb4e773c5f869d5254cc
loicniragire <loic.niragire@gmail.com>
2024-08-14 18:32:58 -0400

    Revert "Init"
    This reverts commit 6350a9f348f82171dbae67aca5634d00b25685ca.

--- GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.IdentityModel.Tokens;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
-global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Authentication;
-global using Microsoft.AspNetCore.Authentication.Google;
